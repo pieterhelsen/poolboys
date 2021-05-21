3c00b4ae3f7afb6ba383aca16dc29c6a7c396248
pieterhelsen <doccie@gmail.com>
2021-05-21 21:33:25 +0200

    Add virtual env and requirements install

--- install.sh
+++ install.sh
@@ -17,6 +17,20 @@ if [ ! -d "${HOME}/chia-blockchain/" ]; then
   read -r CHIA_DIR
 fi
 
+# Create virtual environment
+python3 -m venv venv
+
+# Activate virtual environment
+. ./venv/bin/activate
+
+# Update pip3 to latest version
+python3 -m pip install --upgrade pip
+
+# Install dependencies
+pip3 install wheel && pip3 install -r requirements.txt
+
+# Deactivate virtual environment
+deactivate
 
 # FUNCTIONS
 delegate(){
